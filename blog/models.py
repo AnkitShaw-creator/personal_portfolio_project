07a202569901b88eb5ee06d7cfb6e769e1b55a01
AnkitShaw-creator <ankitshaw667@gmail.com>
2022-07-07 11:49:21 +0530

    Updates

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     datestamp = models.DateField()
     description = models.TextField(max_length=500)
+
+
+    def __str__(self) -> str:
+        return self.title+"||"+str(self.datestamp)
